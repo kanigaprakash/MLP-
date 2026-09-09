8ad677c5409c1c821873d89318ddb2a0fee497c4
kanigaprakash <kanigaprakash06@gmail.com>
2026-09-09 09:25:35 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,16 @@
+student = ("Surya", 20, "AIML", "Python", "AI")
+
+print("Tuple:", student)
+
+print("First:", student[0])
+print("Last:", student[-1])
+
+print("Length:", len(student))
+
+print("Slicing:", student[1:4])
+
+print("Index of AIML:", student.index("AIML"))
+
+numbers = (10, 20, 10, 30, 10)
+
+print("Count of 10:", numbers.count(10))
